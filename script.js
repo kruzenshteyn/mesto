5b2421bfa8133fff995acb79c6d356ca1c1026b8
Aleksei Baev <kruzenshteyn@yandex.ru>
2021-08-16 22:53:56 +0300

    Script
    Загрузка данных со страницы в popup

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ let popupName = document.querySelector('.popup__name');
 let popupAbout = document.querySelector('.popup__about');
 
 function togglePopup(){
+  if(!popup.classList.contains('popup__is-opened')){
+    popupName.value = textProfileName.textContent
+    popupAbout.value = textProfileAbout.textContent;
+  }
   popup.classList.toggle('popup__is-opened');
 };
 
